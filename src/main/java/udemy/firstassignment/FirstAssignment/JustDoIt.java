26466c0e7efb9c671d1a566afcec5e3646bbca08
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-08-05 04:16:15 -0300

    Modified methods to solve

---
+++ src/main/java/udemy/firstassignment/FirstAssignment/JustDoIt.java
@@ -0,0 +1,18 @@
+package udemy.firstassignment.FirstAssignment;
+
+public interface JustDoIt 
+{
+	public abstract long centralElement(long[] vector) throws Exception;
+	
+	public abstract long mostRepeated(long[] vector) throws Exception;
+	
+	public abstract int simplify(int[] vector) throws Exception;
+	
+	public abstract boolean isPalindrome(int[] vector) throws Exception;
+	
+	public abstract int[] commonElements(int[] vector, int[] vector2) throws Exception;
+	
+	public abstract int[] moveZerosToTheRight(int[] vector) throws Exception;
+	
+	public abstract boolean isMagicSquare(int[][] matrix) throws Exception;
+}
---
+++ src/main/java/udemy/firstassignment/FirstAssignment/JustDoItImpl.java
@@ -0,0 +1,40 @@
+package udemy.firstassignment.FirstAssignment;
+
+public class JustDoItImpl implements JustDoIt {
+
+	@Override
+	public long centralElement(long[] vector) throws Exception {
+		throw new RuntimeException("Unimplemented method");
+	}
+
+	@Override
+	public long mostRepeated(long[] vector) throws Exception {
+		throw new RuntimeException("Unimplemented method");
+	}
+
+	@Override
+	public int simplify(int[] vector) throws Exception {
+		throw new RuntimeException("Unimplemented method");
+	}
+
+	@Override
+	public boolean isPalindrome(int[] vector) throws Exception {
+		throw new RuntimeException("Unimplemented method");
+	}
+
+	@Override
+	public int[] commonElements(int[] vector, int[] vector2) throws Exception {
+		throw new RuntimeException("Unimplemented method");
+	}
+
+	@Override
+	public int[] moveZerosToTheRight(int[] vector) throws Exception {
+		throw new RuntimeException("Unimplemented method");
+	}
+
+	@Override
+	public boolean isMagicSquare(int[][] matrix) throws Exception {
+		throw new RuntimeException("Unimplemented method");
+	}
+
+}
