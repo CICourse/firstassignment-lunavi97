b014d74e8a1fdd2282349c2106a0ede2afbc356f
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-08-09 05:39:52 -0300

    Written the test of the move zeros to the right method

--- src/test/java/udemy/firstassignment/FirstAssignment/MoveZerosToTheRightTest.java
+++ src/test/java/udemy/firstassignment/FirstAssignment/MoveZerosToTheRightTest.java
@@ -15,7 +15,56 @@ public class MoveZerosToTheRightTest {
 	}
 
 	@Test
-	public void test() throws Exception {
+	public void test1() throws Exception {
+		int[] vector = {1, 2, 3, 4, 5};
+		
+		Assert.assertEquals(vector, justDoIt.moveZerosToTheRight(vector));
+	}
+	
+	@Test
+	public void test2() throws Exception {
+		int[] vector = {0, 2, 4, 8};
+		int[] vRes = {2, 4, 8, 0};
+		
+		Assert.assertEquals(vRes, justDoIt.moveZerosToTheRight(vector));
+	}
+	
+	@Test
+	public void test3() throws Exception {
+		int[] vector = {0, 0, 0, 8, 4, 8, 2, 1};
+		int[] vRes = {8, 4, 8, 2, 1, 0, 0, 0};
+		
+		Assert.assertEquals(vRes, justDoIt.moveZerosToTheRight(vector));
+	}
+	
+	@Test
+	public void test4() throws Exception {
+		int[] vector = {2, 4, 0, 1, 9, 0, 0, 2, 8, 7, 0, 7, 5, 0, 25, 101};
+		int[] vRes = {2, 4, 1, 9, 2, 8, 7, 7, 5, 25, 101, 0, 0, 0, 0, 0};
+		
+		Assert.assertEquals(vRes, justDoIt.moveZerosToTheRight(vector));
+	}
+	
+	@Test
+	public void test5() throws Exception {
+		int[] vector = {0, 0, 0};
+		
+		Assert.assertEquals(vector, justDoIt.moveZerosToTheRight(vector));
+	}
+	
+	@Test
+	public void test6() throws Exception {
+		int[] vector = null;
+		
+		Assert.assertEquals(null, justDoIt.moveZerosToTheRight(vector));
+	}
+	
+	@Test
+	public void test7() throws Exception {
+		int[] vector = {0, 1, 2, 0, 3, 4, 0};
+		int[] vRes = {1, 2, 3, 4, 0, 0, 0};
+		
+		Assert.assertEquals(vRes, justDoIt.moveZerosToTheRight(vector));
 	}
 
 }
