1c16009922e883f22defd964150ead8f5b4a7b98
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-08-09 06:06:04 -0300

    Written the test of the sum of all odd numbers method

--- src/test/java/udemy/firstassignment/FirstAssignment/SumOddNumberTest.java
+++ src/test/java/udemy/firstassignment/FirstAssignment/SumOddNumberTest.java
@@ -15,7 +15,38 @@ public class SumOddNumberTest {
 	}
 
 	@Test
-	public void test() throws Exception {
+	public void test1() throws Exception {
+		int[] vector = null;
+		
+		Assert.assertEquals(0, justDoIt.sumOddNumber(vector));
+	}
+	
+	@Test
+	public void test2() throws Exception {
+		int[] vector = {1, 2, 3, 4, 5};
+		
+		Assert.assertEquals(9, justDoIt.sumOddNumber(vector));
+	}
+	
+	@Test
+	public void test3() throws Exception {
+		int[] vector = {2, 4, 8, 16, 32, 64, 128};
+		
+		Assert.assertEquals(0, justDoIt.sumOddNumber(vector));
+	}
+	
+	@Test
+	public void test4() throws Exception {
+		int[] vector = {7, 1, 3};
+		
+		Assert.assertEquals(11, justDoIt.sumOddNumber(vector));
+	}
+	
+	@Test
+	public void test5() throws Exception {
+		int[] vector = {9, 2, 3, 4, 3, 1, 11, 9, 8};
+		
+		Assert.assertEquals(36, justDoIt.sumOddNumber(vector));
 	}
 
 }
