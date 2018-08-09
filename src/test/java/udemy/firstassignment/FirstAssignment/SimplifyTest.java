47ac8b8a97c9cabaecb5d162a7d706830f9b85b6
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-08-09 04:53:34 -0300

    Written the test of the simplify method

--- src/test/java/udemy/firstassignment/FirstAssignment/SimplifyTest.java
+++ src/test/java/udemy/firstassignment/FirstAssignment/SimplifyTest.java
@@ -15,7 +15,51 @@ public class SimplifyTest {
 	}
 
 	@Test
-	public void test() throws Exception {
+	public void test1() throws Exception {
+		int[] vector = {1, 2, 3};
+		
+		Assert.assertEquals(vector, justDoIt.simplify(vector));
+	}
+	
+	@Test
+	public void test2() throws Exception {
+		int[] vector = {5, 1, 7, 25, 3};
+		int[] simplifiedVector = {1, 3, 5, 7, 25};
+		
+		Assert.assertEquals(simplifiedVector, justDoIt.simplify(vector));
+	}
+	
+	@Test
+	public void test3() throws Exception {
+		int[] vector = {4, 5, 10, 10, 11, 11};
+		int[] simplifiedVector = {4, 5, 10, 11};
+		
+		Assert.assertEquals(simplifiedVector, justDoIt.simplify(vector));
+	}
+	
+	@Test
+	public void test4() throws Exception {
+		int[] vector = {9, 2, 3, 4, 10, 1, 9, 10, 2};
+		int[] simplifiedVector = {1, 2, 3, 4, 9, 10};
+		
+		Assert.assertEquals(simplifiedVector, justDoIt.simplify(vector));
+	}
+	
+	@Test
+	public void test5() throws Exception {
+		int[] vector = {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4};
+		int[] simplifiedVector = {4};
+		
+		Assert.assertEquals(simplifiedVector, justDoIt.simplify(vector));
+	}
+	
+	@Test
+	public void test6() throws Exception {
+		int[] vector = {9, 9, 2, 3, 5, 2, 2, 4, 3, 8, 3, 9, 6};
+		int[] simplifiedVector = {2, 3, 4, 5, 6, 8, 9};
+		
+		Assert.assertEquals(simplifiedVector, justDoIt.simplify(vector));
+		
 	}
 
 }
